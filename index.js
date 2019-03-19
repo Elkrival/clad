bf86d67e2fe8eba48c5b4e6d529833d2a5203713
Ivan <ivan@Ivans-MacBook-Pro.local>
2019-03-19 17:59:54 -0400

    Title: Puppeteer Updates * Added configuration to parse snackbar * Added configuration to select and differentiate from dialog boxes * Added evaluation for dropdown selections * Added workflows and evaluations for csv file

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const blacklist = ['www.google-analytics.com', '/gtag/js', 'ga.js', 'analytics.j
 let browser
 let test;
 let campaignSubject;
+let pageOrigin;
+let vendorSubject;
 async function initScrape(){
     try {
         console.log('initializing headless puppeteer... ( ͡° ͜ʖ ͡°)')
-        browser = await pupp.launch({ headless: false, args: ['--start-maximized'] });
+        browser = await pupp.launch({ headless: false, args: ['--window-size=1080,1800', '--start-maximized'] });
         console.log('launching browser page...')
         const page = await browser.newPage();
-        await page.setViewport({ height: 1200, width: 1200 })
+        // await page.setViewport({ height: 1080, width: 1800 })
         await page.setRequestInterception(true);
         // await page.setViewport({ width: 1280, height: 1000 })
         await page._client.send('Page.setDownloadBehavior', {
@@ -34,12 +36,18 @@ async function initScrape(){
                 }else if(eventType === 'response') {
                     if(eventFilterFunc.url().includes('client')) {
                         // resolves data result from request
-                        let data = await eventFilterFunc.json().then(res => res);
-                        console.log(data.hasOwnProperty('campaignTable'), 'verification');
-                        console.log(data, '================ Data ================$$$$$$')
-                         if(data.hasOwnProperty('campaignTable')) {
-                            test = data
-                            campaignSubject = data.campaignTable[1];
+                        if(eventFilterFunc.url().includes('/client/rfis/templates/')) {
+                            let data = await eventFilterFunc.json().then(res => res);
+                            console.log(data.hasOwnProperty('campaignTable'), 'verification');
+                            console.log(data, '================ Data ================$$$$$$')
+                            if(data.hasOwnProperty('campaignTable')) {
+                                test = data
+                                campaignSubject = data.campaignTable[1];
+                            }
+                        }
+                        if(eventFilterFunc.url().includes('client/group/data/')) {
+                            await eventFilterFunc.json().then(res => vendorSubject =  res.result);
+                            
                         }
                         
                     }
@@ -51,9 +59,9 @@ async function initScrape(){
         })
     await page.goto('http://localhost:8080/#/auth/login');
     console.log(page.url())
-    await page.click('#email');
+    await page.click('#loginEmail');
     await page.keyboard.type(process.env.USERNAME);
-    await page.click('#password');
+    await page.click('#loginPassword');
     await page.keyboard.type(process.env.PASSWORD);
     await page.click('#enter');
     await page.waitForSelector('#rfis')
@@ -68,7 +76,7 @@ async function initScrape(){
     await page.click('mat-row:nth-child(2) > mat-cell.scmp-table-wideCol.mat-cell.cdk-column-campaign.mat-column-campaign.ng-star-inserted > a')
     // ============== Go back page tests after all network calls are completed =============
     await page.goBack({ networkidle0: true });
-    await page.waitFor(4000);
+    await page.waitFor(200);
     setTimeout(() => {
         console.log(page.url() + ' ======URL=====')
     }, (500));
@@ -98,18 +106,19 @@ async function initScrape(){
     await page.focus('#campaigns_input > input');
     await page.keyboard.type('leather');
     let searchResult = await page.evaluate((query) => document.querySelector('mat-cell').innerText.toLowerCase().includes(query), 'leather')
+    console.log(searchResult + ' Macy Gray =============== ')
     let filteredTable = await page.evaluate(() => document.querySelector('mat-table').childElementCount);
     console.log(entireTable > filteredTable, entireTable + ' entire table size ' + filteredTable + ' filtered table size');
     await page.click("#campaigns_input > input", {clickCount: 3});
-    await page.waitFor(2000);
+    await page.waitFor(500);
     await page.keyboard.press('Backspace');
     await page.focus('mat-header-cell > div > button');
     entireTable = await page.evaluate(() => document.querySelector('mat-table').children[1].innerText.split('Active')[0]);
     console.log(entireTable + ' Sorting Value');
     await page.click('mat-header-cell > div > button');
-    await page.waitFor(2000);
+    await page.waitFor(500);
     await page.click('mat-header-cell > div > button');
-    await page.waitFor(2000);
+    await page.waitFor(500);
     filteredTable = await page.evaluate(() => document.querySelector('mat-table').children[1].innerText.split('Active')[0]);
     console.log(entireTable.valueOf() === filteredTable.valueOf() + ' Comparison of table values ===========');
     await fs.unlink(__dirname + '/Campaigns.csv', (err) =>{
@@ -119,20 +128,65 @@ async function initScrape(){
     await page.waitFor(5000);
     // ================== Navigate from Table Item ==============
     await page.evaluate(() => document.querySelector('mat-table > mat-row:nth-child(9)').children[0].children[0].click());
-    await page.waitFor(2000);
+    await page.waitFor(500);
     console.log(campaignSubject);
 
     // Campaign Page evaluator tests
     let header = await page.evaluate(() => document.querySelector('.scmp-page-title--secondary').innerText.toLowerCase());
     console.log(header === campaignSubject.campaign_name.toLowerCase());
     await page.click('#addVendor');
+    await page.waitFor(500);
+    // await page.click('#selectVendor > div');
+    // await page.waitFor(2000);
+    // await page.evaluate(() => document.querySelector('.mat-select-content').children[0].click());
+    // await page.waitFor(2000);
+    // await page.click('mat-dialog-container');
+    // await page.waitFor(2000);
+    // await page.click('#vendorSubmit');
+    await page.click('#close');
+    await page.waitFor(500);
+    pageOrigin = await page.url()
+    let reg = await page.evaluate(() => document.querySelector('.pointer-end').innerText);
+    console.log(reg, campaignSubject.groups_responding);
+    console.log(parseFloat(reg) > parseFloat(campaignSubject.groups_responding))
+    await page.waitFor(500);
+    await page.click('#surveyTable > mat-row:nth-child(2) > mat-cell.mat-cell.cdk-column-vendor.mat-column-vendor.ng-star-inserted > a');
+    await page.waitFor(500);
+    let navigated = await page.url();
+    console.log(navigated === pageOrigin)
+    await page.waitFor(500);
+    await page.click('#addCampaign');
+    await page.waitFor(500);
+    await page.click('#selectCampaign > div');
+    await page.waitFor(500);
+    await page.evaluate(() => document.querySelector('mat-option').children[0].click());
+    await page.waitFor(500);
+    await page.click('#close');
+    await page.click('#close');
+    const { answers, counts, details, group, pending, roles, users } = vendorSubject; 
+    await page.waitFor(500);
+    let vendorCampaignList = await page.evaluate((groupList) => document.querySelector('#groupCampaignList').childElementCount - 2 === groupList, details.length);
+    console.log(vendorCampaignList);
+    await page.waitFor(500);
+    await page.click('#addCampaign');
+    await page.waitFor(500);
+    await page.click('#selectCampaign > div');
+    await page.waitFor(500);
+    await page.evaluate(() => document.querySelector('mat-option').children[0].click());
+    await page.waitFor(500);
+    await page.click('#close');
+    await page.click('#close');
+    await page.waitFor(1000);
+    await page.click('#inviteUser');
+    await page.focus('form > div > div > input');
+    await page.keyboard.type(process.env.INVITE_EMAIL);
+    await page.waitFor(500);
+    await page.evaluate(() => document.querySelector('mat-radio-group').children[0].firstElementChild.click());
     await page.waitFor(2000);
-    await page.click('.scmp-select');
-    await page.evaluate(() => document.querySelector('.mat-select-content').children[0].click());
+    await page.click('#sendInvite');
     await page.waitFor(2000);
-    await page.click('mat-dialog-container');
-    await page.waitFor(2000);
-    await page.click('#vendorSubmit');
+    let snackCampaign = await page.evaluate(() => document.querySelector('snack-bar-container').innerText.includes('has been invited'));
+    console.log(snackCampaign + ' snack - bar - campaign')
 
     // await browser.close()
     await console.log('shutting down puppeteer... ʕ •ᴥ•ʔ');
